1c83079ce4827c89af936b8bf832b7570f191fd5
Jonathan Harden <jfharden@gmail.com>
2024-01-28 14:49:40 +0000

    Add tests for validate command

--- src/commands/validate.rs
+++ src/commands/validate.rs
@@ -10,3 +10,32 @@ pub fn validate(option_rom: OptionRom) -> Result<String, String> {
         Err(e) => Err(format!("{}", e)),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_helpers::load_option_rom_fixture;
+
+    #[test]
+    fn validate_with_valid_rom() -> Result<(), String> {
+        let option_rom = load_option_rom_fixture("pc.boot.valid")?;
+
+        match validate(option_rom) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(format!("Expected Ok but got error {}", e)),
+        }
+    }
+
+    #[test]
+    fn validate_with_bad_checksum() -> Result<(), String> {
+        let option_rom = load_option_rom_fixture("pc.boot.invalid_checksum")?;
+
+        match validate(option_rom) {
+            Ok(_) => Err("Expected an error validating checksum on pc.boot.invalid but got Ok".into()),
+            Err(message) => {
+                assert_eq!(message, "Option Rom Checksum Invalid. Requires checksum byte F1");
+                Ok(())
+            },
+        }
+    }
+}
